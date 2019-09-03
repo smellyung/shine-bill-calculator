b5d772a12820545bd82d1be220b8d8ad5df07b90
smellyung <kelly.ung@centrica.com>
2019-09-03 15:48:24 +0100

    create enter meter read component

---
+++ src/EnterMeterRead.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+export default function EnterMeterRead() {
+    const [value, setValue] = useState('');
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        console.log(value)
+    }
+    return (
+        <div className='enter-meter-read'>
+            <h1>Please enter your (Gas/Electricity) Meter Read:</h1>
+            <div>
+                <input value={value} onChange={handleChange}/>
+                <Link to={{ pathname: '/gas-summary', value}} >
+                    <button>
+                        Submit
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
+}
